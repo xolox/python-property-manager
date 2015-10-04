48c8bd631fd01d8cc6e70de26497f86848da8282
Peter Odding <peter@peterodding.com>
2015-10-04 16:51:30 +0200

    Remove accidentally committed docs/index.py file

--- docs/index.py
+++
@@ -1,7 +0,0 @@
-# Useful property variants for Python programming.
-#
-# Author: Peter Odding <peter@peterodding.com>
-# Last Change: October 4, 2015
-# URL: https://property-manager.readthedocs.org
-
-
